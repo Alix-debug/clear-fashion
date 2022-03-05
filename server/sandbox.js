d163e80a02954d257b34698704f0d1b02476a494
Alix-debug <alix.petitcol@yahoo.fr>
2022-03-05 22:07:52 +0100

    Save all products scraped in variable

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,19 +1,36 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./sources/dedicatedbrand');
+const montlimart = require('./sites/montlimart');
+const adresseParis =require("./sites/adresseParis");
+const Eshop = [
+    'https://www.dedicatedbrand.com/en/men/news',
+    'https://www.montlimart.com/toute-la-collection.html',
+    'https://adresse.paris/630-toute-la-collection'
+]
+let moduleRequired =[
+  dedicatedbrand,
+  montlimart, 
+  adresseParis
+]
+let all_produts=[]
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
-  try {
-    console.log(`🕵️‍♀️  browsing ${eshop} source`);
+async function sandbox () {
+  
+  for(let i=0; i<Eshop.length;i++){
+    try {
+      console.log(`🕵️‍♀️  browsing ${Eshop[i]} source`);
+      products= await moduleRequired[i].scrape(Eshop[i]);
+      console.log(products);
+      console.log("done");
 
-    const products = await dedicatedbrand.scrape(eshop);
+      all_produts.push(products);
+    } 
+    catch (e) {
+      console.error(e);
+    }
 
-    console.log(products);
-    console.log('done');
-    process.exit(0);
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
-  }
+  };
+  
 }
 
 const [,, eshop] = process.argv;
